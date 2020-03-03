02ac2ada3d687d355e5e9f40d7d6d6fbce27f512
at-vuongtran <vuong.tran@asiantech.vn>
2020-03-03 17:33:14 +0700

    create table users, roles, permission

---
+++ db/migrate/20200303095628_create_users.rb
@@ -0,0 +1,17 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.references :roles, precence: true
+      t.string :name
+      t.string :email, unique: true
+      t.string :password_digest
+      t.string :remember_digest
+      t.boolean :activated, default: false
+      t.datetime :activated_at
+      t.string :reset_digest
+      t.datetime :reset_send_at
+
+      t.timestamps
+    end
+  end
+end
