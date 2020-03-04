41d0f750c279c586231a3823043840877993b68c
at-vuongtran <vuong.tran@asiantech.vn>
2020-03-04 09:45:19 +0700

    define admin

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,15 @@
+class Admin::UsersController < ApplicationController
+  def index; end
+
+  def show; end
+
+  def edit; end
+
+  def update; end
+
+  def destroy; end
+
+  def new; end
+
+  def create; end
+end
