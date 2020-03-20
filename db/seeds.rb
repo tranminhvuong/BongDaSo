040ae939c9642de628dc16d5c1806ec4f880fcf3
at-vuongtran <vuong.tran@asiantech.vn>
2020-03-20 15:04:39 +0700

    fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
 Role.create(name: 'admin')
 Role.create(name: 'ctv')
-User.create(name: 'Ho Sy Ty', email: 'admin01@gmail.com', password: '12345678', password_confirmation: '12345678', role_id :Role.first.id)
+User.create(name: 'Ho Sy Ty', email: 'admin01@gmail.com', password: '12345678', password_confirmation: '12345678', role_id: Role.first.id)
