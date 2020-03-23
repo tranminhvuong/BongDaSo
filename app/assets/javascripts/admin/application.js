3f629e33651ea5ea58ba2d3017fad4f5c681eece
at-vuongtran <vuong.tran@asiantech.vn>
2020-03-23 13:54:06 +0700

    fix action text and add link cancel in edit

--- app/assets/javascripts/admin/application.js
+++ app/assets/javascripts/admin/application.js
@@ -1,12 +1,6 @@
-//= require jquery
-//= require rails-ujs
+//= require jquery3
 //= require popper
+//= require rails-ujs
+//= require bootstrap
 //= require bootstrap-sprockets
-//= require trix
-
-
-$(document).ready(function () {
-    $('#sidebarCollapse').on('click', function () {
-        $('#sidebar').toggleClass('active');
-    });
-});
+//= require_tree .
